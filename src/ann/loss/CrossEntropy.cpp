1019baa3342ee253c41594d27257b52135243af6
TiDz <nguyentinvs123@Gmail.com>
2024-11-16 15:15:53 +0700

    update cross-e-forward

--- src/ann/loss/CrossEntropy.cpp
+++ src/ann/loss/CrossEntropy.cpp
@@ -24,12 +24,15 @@ CrossEntropy::~CrossEntropy() {}
 
 double CrossEntropy::forward(xt::xarray<double> X, xt::xarray<double> t) {
   // Todo CODE YOUR
-  int N = X.shape()[0];
-  m_aYtarget = t;
-  m_aCached_Ypred = X;
-  xt::xarray<double> logYpred = xt::log(m_aCached_Ypred + 1e-7);
-  xt::xarray<double> loss = xt::sum(-m_aYtarget * logYpred, -1)/N;
-  return loss[0];
+  // int N = X.shape()[0];
+  // m_aYtarget = t;
+  // m_aCached_Ypred = X;
+  // xt::xarray<double> logYpred = xt::log(m_aCached_Ypred + 1e-7);
+  // xt::xarray<double> loss = xt::sum(-m_aYtarget * logYpred, -1)/N;
+  // return loss[0];
+  this->m_aYtarget = t;
+  this->m_aCached_Ypred = X;
+  return cross_entropy(X, t, m_eReduction == REDUCE_MEAN);
 }
 xt::xarray<double> CrossEntropy::backward() {
   // Todo CODE YOUR
